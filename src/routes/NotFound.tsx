926283a8eb3d9a94880bcd8521fb77a6cd02ff2a
Tomas_Zagorskis <tomzagos@gmail.com>
2023-03-18 22:43:02 +0200

    feat: implement 404 page

--- src/routes/NotFound.tsx
+++ src/routes/NotFound.tsx
@@ -1,4 +1,18 @@
+import { Link } from 'react-router-dom';
+
+import classes from './NotFound.module.css';
+
 const NotFound = () => {
-  return <div>NotFound</div>;
+  return (
+    <article className={classes['not-found']}>
+      <h2>
+        404 <br />
+        PAGE NOT FOUND
+      </h2>
+      <Link to={'/'} className={classes.btn}>
+        Home
+      </Link>
+    </article>
+  );
 };
 export default NotFound;
