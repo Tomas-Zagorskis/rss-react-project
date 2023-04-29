e9051e760097f62bb54ebf046aeedd9e57ebfadb
Tomas Zagorskis <tomzagos@gmail.com>
2023-04-29 14:33:21 +0300

    fix: handling error and refactor on TypeScript

--- server.ts
+++ server.ts
@@ -1,5 +1,6 @@
 import fs from 'node:fs/promises';
 import express from 'express';
+import { ViteDevServer } from 'vite';
 
 // Constants
 const isProduction = process.env.NODE_ENV === 'production';
@@ -13,7 +14,7 @@ const templateHtml = isProduction ? await fs.readFile('./dist/client/index.html'
 const app = express();
 
 // Add Vite or respective production middlewares
-let vite;
+let vite: ViteDevServer;
 if (!isProduction) {
   const { createServer } = await import('vite');
   vite = await createServer({
@@ -34,34 +35,41 @@ app.use('*', async (req, res) => {
   try {
     const url = req.originalUrl;
 
-    let template;
-    let render;
-    if (!isProduction) {
-      // Always read fresh template in development
-      template = await fs.readFile('./index.html', 'utf-8');
-      template = await vite.transformIndexHtml(url, template);
-      render = (await vite.ssrLoadModule('/src/entry-server.tsx')).render;
-    } else {
-      template = templateHtml;
-      render = (await import('./dist/server/entry-server.js')).render;
-    }
+    let template: string;
+    let render: (
+      arg0: string,
+      arg1: { onShellReady(): void; onShellError(error: any): void; onAllReady(): void }
+    ) => PromiseLike<{ pipe: any }> | { pipe: any };
+
+    // Always read fresh template in development
+    template = await fs.readFile('./index.html', 'utf-8');
+    template = await vite.transformIndexHtml(url, template);
+    render = (await vite.ssrLoadModule('/src/entry-server.tsx')).render;
 
     const html = template.split(`<!--app-html-->`);
 
     const { pipe } = await render(url, {
       onShellReady() {
+        res.setHeader('content-type', 'text/html');
         res.write(html[0]);
         pipe(res);
       },
+      onShellError(_) {
+        res.statusCode = 500;
+        res.setHeader('content-type', 'text/html');
+        res.send('<h1>Something went wrong</h1>');
+      },
       onAllReady() {
-        res.write(html[0] + html[1]);
+        res.write(html[1]);
         res.end();
       },
     });
   } catch (e) {
-    vite?.ssrFixStacktrace(e);
-    console.log(e.stack);
-    res.status(500).end(e.stack);
+    if (e instanceof Error) {
+      vite?.ssrFixStacktrace(e);
+      console.log(e.stack);
+      res.status(500).end(e.stack);
+    }
   }
 });
 
